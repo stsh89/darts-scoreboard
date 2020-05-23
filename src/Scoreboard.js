54efd38e9bf76aa485dc313d106331faeeaf0a84
Stanislav Shandyga <ssh@anadeainc.com>
2020-05-23 14:06:39 +0300

    Add initial scoreboard functionality

--- src/Scoreboard.js
+++ src/Scoreboard.js
@@ -1,29 +1,103 @@
 import * as React from 'react'
 
 export default function Scoreboard() {
-  const [player1Score, setPlaer1Score] = React.useState(301)
-  const [player2Score, setPlaer2Score] = React.useState(301)
+  const [player1Score, setPlayer1Score] = React.useState(301)
+  const [player2Score, setPlayer2Score] = React.useState(301)
+  const [player1ScoreList, setPlayer1ScoreList] = React.useState([])
+  const [player2ScoreList, setPlayer2ScoreList] = React.useState([])
+  const [turn, setTurn] = React.useState('player1')
+  const [scoreInputLabel, setScoreInputLabel] = React.useState('Player1')
+  const [scoreInputValue, setScoreInputValue] = React.useState('')
 
-  const controls = [1,2,3,4,5,6,7,8,9,0,'enter'].map((value) => {
+  const numberControlClick = (value) => {
+    setScoreInputValue(`${scoreInputValue}${value}`)
+  }
+
+  const numberControls = [1,2,3,4,5,6,7,8,9,0].map((value) => {
     return (
-      <button key={`control-${value}`}>{value}</button>
+      <button key={`control-${value}`} onClick={numberControlClick.bind(this, value)}>{value}</button>
     )
   })
 
+  const removeControlClick = () => {
+    setScoreInputValue(scoreInputValue.substring(0, scoreInputValue.length - 1))
+  }
+
+  const enterControlClick = () => {
+    changeTurnWithGuard()
+  }
+
+  const changeTurnWithGuard = () => {
+    const score = parseInt(scoreInputValue)
+
+    if (!isNaN(score)) {
+      changeTurn(score)
+    }
+  }
+
+  const reset = () => {
+    setPlayer1Score(301)
+    setPlayer1ScoreList([])
+    setPlayer2Score(301)
+    setPlayer2ScoreList([])
+    setTurn('player1')
+    setScoreInputLabel('Player1')
+    setScoreInputValue('')
+  }
+
+  const updateScore = (e) => {
+    setScoreInputValue(e.target.value)
+  }
+
+  const changeTurn = (score) => {
+    switch (turn) {
+      case 'player1':
+        player1ScoreList.push(score)
+        setPlayer1ScoreList(player1ScoreList)
+        setTurn('player2')
+        setScoreInputLabel('Player2')
+        setPlayer1Score(player1Score - score)
+        break
+      case 'player2':
+        player2ScoreList.push(score)
+        setPlayer2ScoreList(player2ScoreList)
+        setTurn('player1')
+        setScoreInputLabel('Player1')
+        setPlayer2Score(player2Score - score)
+        break
+    }
+
+    setScoreInputValue('')
+  }
+
+  const setScore = (e) => {
+    if (e.key === 'Enter') {
+      changeTurnWithGuard()
+    }
+  }
+
   return (
     <div className="scoreboard">
-      <button>Reset</button>
+      <button onClick={reset}>Reset</button>
       <div className="score">
         <span>{player1Score}</span>
         <span>:</span>
         <span>{player2Score}</span>
       </div>
-      <div className="score-inputs">
-        <span><input type="number" /></span>
-        <span><input type="number" /></span>
+      <div className="score-input">
+        <label htmlFor="scoreInput">{scoreInputLabel}</label>
+        <input
+          id="scoreInput"
+          type="number"
+          onKeyDown={setScore}
+          value={scoreInputValue}
+          onChange={updateScore}
+        />
       </div>
       <div className="controls">
-        {controls}
+        {numberControls}
+        <button onClick={removeControlClick}>Remove</button>
+        <button onClick={enterControlClick}>Enter</button>
       </div>
     </div>
   )
